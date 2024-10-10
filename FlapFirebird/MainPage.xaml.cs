2983beacd7b1b7c9b794e7aed1e49642ff19a39e
Anabovo <sousabovoana@gmail.com>
2024-10-10 08:50:57 -0300

    10/10

--- FlapFirebird/MainPage.xaml.cs
+++ FlapFirebird/MainPage.xaml.cs
@@ -2,12 +2,36 @@
 
 public partial class MainPage : ContentPage
 {
-
+    
+	const int gravidade=30;
+	const int tempoEntreFrames=1000;
+	bool estaMorto=false;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		
 	}
+
+	void AplicaGravidade()
+	{
+		ImagemFirebird.TranslationY+=gravidade;
+	}
+
+	async Task Desenhar()
+	{
+		while (! estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenhar();
+    }
 
 }
 
