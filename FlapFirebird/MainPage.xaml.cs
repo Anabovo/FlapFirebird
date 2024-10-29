0f4fb606c327e91368e4abdedeefa5d044bf8dce
Anabovo <sousabovoana@gmail.com>
2024-10-29 11:33:56 -0300

    dia 29/10

--- FlapFirebird/MainPage.xaml.cs
+++ FlapFirebird/MainPage.xaml.cs
@@ -4,93 +4,133 @@ namespace FlapFirebird;
 
 public partial class MainPage : ContentPage
 {
+	int velocidade=20;
 	const int gravidade=30;
+	const int forcaPulo=45;
+	const int maxTempoPulando=2;
+
+
+	const int aberturaMinima=200;
+
+
+    int score=0;
 	const int tempoEntreFrames=75;
-	bool estaMorto=true;
 	double larguraJanela=0;
 	double alturaJanela=0;
-	int velocidade=20;
-	const int maxTempoPulando=2;
-	int tempoPulando=0;
+	bool estaMorto=true;
 	bool estaPulando=false;
-	const int forcaPulo=45;
-	const int aberturaMinima=200;
-	int score=0;
-
+	int tempoPulando=0;
+	
+	//---------------------------------------------------------------------------------------
+	
+	
 	public MainPage()
 	{
 		InitializeComponent();
 		
 	}
 
+	//-----------------------------------------------------------------------------------------
+
 	protected override void OnAppearing()
 	{
 		base.OnAppearing();
 	}
 
-	void AplicaGravidade()
-	{
-		ImagemFirebird.TranslationY+=gravidade;
-	}
+	//-----------------------------------------------------------------------------------------
 
+
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+
+        larguraJanela = width;
+        alturaJanela = height;
+        if (height > 0)
+        {
+            ImagemCanoAlto.HeightRequest  = height - ImagemChao.HeightRequest;
+            ImagemCanoBaixo.HeightRequest = height - ImagemChao.HeightRequest;
+        }
+    }
+	//-------------------------------------------------------------------------------------------
+	
+	void Inicializar()
+	{
+		estaMorto=false;
+		ImagemFirebird.TranslationY=0;
+	}
+	//---------------------------------------------------------------------------------------------------------
+	
 	async Task Desenhar()
 	{
 		while (! estaMorto)
 		{
 			if (estaPulando)
 			    AplicaPulo();
+
 			else 
 			    AplicaGravidade();
 			    GerenciaCanos();
+
 			if (VerificaColisao())
 			{
 				estaMorto=true;
+				SoundHelper.Play("gameover.wav");
+				labelGameOverScore.Text = "Você passou por\n" + score + " canos";
 				FrameGameOver.IsVisible=true;
 				break;
 			}
 			await Task.Delay(tempoEntreFrames);
 		}
 	}
+	
+	//---------------------------------------------------------------------------------------------------------------
 
-	protected override void OnSizeAllocated(double w, double h)
-	{
-		base.OnSizeAllocated(w, h);
-		larguraJanela = w;
-		alturaJanela = h;
-	}
 
 	void GerenciaCanos()
 	{
-		ImagemCanoAlto.TranslationX-=velocidade;
-		ImagemCanoBaixo.TranslationX-=velocidade;
-		if (ImagemCanoBaixo.TranslationX<= -larguraJanela)
+		ImagemCanoAlto.TranslationX -= velocidade;
+		ImagemCanoBaixo.TranslationX -= velocidade;
+		if (ImagemCanoBaixo.TranslationX < -larguraJanela)
 	    {
-			var alturaMax=-10;
-			var alturaMin=-ImagemCanoBaixo.HeightRequest;
-			ImagemCanoAlto.TranslationY=Random.Shared.Next((int)alturaMin, (int)alturaMax);
-			ImagemCanoBaixo.TranslationY=ImagemCanoAlto.TranslationY=aberturaMinima;
 			ImagemCanoBaixo.TranslationX=0;
 			ImagemCanoAlto.TranslationX=0;
+
+			var alturaMax=-10;
+			var alturaMin=-ImagemCanoBaixo.HeightRequest;
+
+			ImagemCanoAlto.TranslationY=Random.Shared.Next((int)alturaMin, (int)alturaMax);
+			ImagemCanoBaixo.TranslationY=ImagemCanoAlto.TranslationY=aberturaMinima;
+			
 			score++;
 			labelScore.Text = "Score:" + score.ToString("D3");
-			if (score % 2 == 0)
+			if (score % 4 == 0)
 			    velocidade++;
 		}
 	}
+	
+	//-------------------------------------------------------------------------------------------------------
 
-	void OnGameOverClicked(object s, TappedEventArgs a)
+
+    void AplicaGravidade()
 	{
-		FrameGameOver.IsVisible=false;
-		Inicializar();
-		Desenhar();
+		ImagemFirebird.TranslationY += gravidade;
+	}
+    //--------------------------------------------------------------------------------------------------------
+	
+	void AplicaPulo()
+	{
+		ImagemFirebird.TranslationY -= forcaPulo;
+		tempoPulando++;
+		if (tempoPulando >= maxTempoPulando)
+		{
+			tempoPulando=0;
+			
+		}
 	}
 
-	void Inicializar()
-	{
-		estaMorto=false;
-		ImagemFirebird.TranslationY=0;
-	}
-
+	//----------------------------------------------------------------------------------------------------------
+	
 	bool VerificaColisao()
 	{
 		if(!estaMorto)
@@ -106,48 +146,38 @@ public partial class MainPage : ContentPage
 		return false;
 	}
 
-	bool VerificaColisaoTeto()
+	//-----------------------------------------------------------------------------------------------------------------------
+	
+	void OnGameOverClicked(object s, TappedEventArgs a)
 	{
-		var minY = -alturaJanela/2;
-		if (ImagemFirebird.TranslationY <= minY)
-		    return true;
-		else
-		    return false;
+		FrameGameOver.IsVisible=false;
+		Inicializar();
+		Desenhar();
 	}
 
-	bool VerificaColisaoChao()
-	{
-	    var maxY= alturaJanela/2 - ImagemChao.HeightRequest;
-		if(ImagemFirebird.TranslationY >= maxY)
-		   return true;
-		else
-		   return false;
+    //-----------------------------------------------------------------------------------------------------------------------------------
+
+	bool VerificaColisaoCano()
+    {
+    if (VerificaColisaoCanoBaixo() || VerificaColisaoCanoalto())
+      return true;
+    else
+       return false;
     }
 
-	void AplicaPulo()
-	{
-		ImagemFirebird.TranslationY -= forcaPulo;
-		tempoPulando++;
-		if (tempoPulando >= maxTempoPulando)
-		{
-			estaPulando=false;
-			tempoPulando=0;
-		}
-	}
-
-	void OnGridClicked(object s, TappedEventArgs a)
-	{
-		estaPulando=true;
-	}
+    //-------------------------------------------------------------------------------------------------------------------------------
+	
 
 	bool VerificaColisaoCanoalto()
 	{
-	   var posHPardal = (larguraJanela/2)-(ImagemFirebird.WidthRequest/2);
-	   var posVPardal = (larguraJanela/2)-(ImagemFirebird.HeightRequest/2);
+	   var posHPardal = (larguraJanela - 50) - (ImagemFirebird.WidthRequest / 2);
+	   var posVPardal = (alturaJanela / 2) - (ImagemFirebird.HeightRequest / 2) + ImagemFirebird.TranslationY;
 
-	   if(posHPardal >= Math.Abs(ImagemCanoAlto.TranslationX)-ImagemCanoAlto.WidthRequest&&
-	      posHPardal <= Math.Abs(ImagemCanoAlto.TranslationX)+ImagemCanoAlto.WidthRequest&&
-		  posVPardal <= ImagemCanoAlto.HeightRequest + ImagemCanoAlto.TranslationY)
+	   if(
+		  posHPardal >= Math.Abs(ImagemCanoAlto.TranslationX) - ImagemCanoAlto.WidthRequest &&
+	      posHPardal <= Math.Abs(ImagemCanoAlto.TranslationX) + ImagemCanoAlto.WidthRequest &&
+		  posVPardal <= ImagemCanoAlto.HeightRequest + ImagemCanoAlto.TranslationY
+		 )
 		  {
 			return true;
 		  }
@@ -157,14 +187,18 @@ public partial class MainPage : ContentPage
 		  }
 	}
 
+	//-------------------------------------------------------------------------------------------------------------------
+	
 	bool VerificaColisaoCanoBaixo()
 	{
-		var posHPardal = (larguraJanela/2)-(ImagemFirebird.WidthRequest/2);
-		var posVPardal = (alturaJanela/2)+(ImagemFirebird.HeightRequest/2)+ImagemFirebird.TranslationY;
-		var yMaxCano = ImagemCanoAlto.HeightRequest+ImagemCanoAlto.TranslationY+aberturaMinima;
-		if(posHPardal >= Math.Abs(ImagemCanoBaixo.TranslationX)-ImagemCanoBaixo.WidthRequest&&
-		   posHPardal <= Math.Abs(ImagemCanoBaixo.TranslationX)+ImagemCanoBaixo.WidthRequest&&
-		   posVPardal >= yMaxCano)
+		var posHPardal = larguraJanela - 50 - ImagemFirebird.WidthRequest / 2;
+		var posVPardal = (alturaJanela / 2) + (ImagemFirebird.HeightRequest / 2) + ImagemFirebird.TranslationY;
+		var yMaxCano = ImagemCanoAlto.HeightRequest + ImagemCanoAlto.TranslationY + aberturaMinima;
+		if(
+			posHPardal >= Math.Abs(ImagemCanoBaixo.TranslationX) - ImagemCanoBaixo.WidthRequest &&
+		    posHPardal <= Math.Abs(ImagemCanoBaixo.TranslationX) + ImagemCanoBaixo.WidthRequest &&
+		    posVPardal >= yMaxCano
+		  )
 		   {
 			return true;
 		   }
@@ -173,6 +207,39 @@ public partial class MainPage : ContentPage
 			return false;
 		   }
 	}
+	
+	//--------------------------------------------------------------------------------------------------------------------------------
+	
+	bool VerificaColisaoChao()
+	{
+	    var maxY = alturaJanela / 2 - ImagemChao.HeightRequest;
+		if(ImagemFirebird.TranslationY >= maxY - ImagemChao.HeightRequest)
+		   return true;
+		else
+		   return false;
+    }
+
+	//------------------------------------------------------------------------------------------------------------------------
+	
+	bool VerificaColisaoTeto()
+	{
+		var minY = alturaJanela / 2;
+		if (ImagemFirebird.TranslationY <= 
+		.minY)
+		    return true;
+		else
+		    return false;
+	}
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+	void OnGridClicked(object s, TappedEventArgs a)
+	{
+		estaPulando=true;
+	}
+
+	
+
+	
 
 
 
